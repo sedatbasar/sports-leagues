bfb60dfc97247ce594c63adb456dce781de81b70
sedatbasar <sedatbasar@hotmail.com>
2025-08-22 17:02:10 +0300

    Fix react-router base config

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,5 +4,5 @@ export default {
   // Enable SPA mode for GitHub Pages static hosting
   ssr: false,
   // Set base path for GitHub Pages (will be your repo name)
-  basename: process.env.NODE_ENV === 'production' ? '/sports-leagues' : '',
+  basename: process.env.NODE_ENV === 'production' ? '/sports-leagues' : '/',
 } satisfies Config;
